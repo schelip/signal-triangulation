b8c6fd028ca786bfa3173dc0571f0200849b959e
schelip <fcominscheffel@gmail.com>
2023-01-31 22:29:09 -0300

    bugfixes, triangulate function, refactors and example.sh

--- example.sh
+++ example.sh
@@ -1,4 +1,5 @@
-[
+touch ./receivers_data_example.json
+echo '[
     {
         "x": 1.55,
         "y": 17.63,
@@ -25,8 +26,15 @@
     },
     {
         "x": 9.15,
-        "y": 4.64,
-        "p0": -31.2,
-        "L": 1.4
+        "y": 12,
+        "p0": -33.0,
+        "L": 1.5
     }
-]
+]' > receivers_data_example.json
+
+printf 'Exemplo 1 (3.00, 3.00)\n'
+py sinais.py receivers_data_example.json -v -48.4 -50.6 -32.2 -47.4 -46.3 \
+
+printf '\nExemplo 2 (0.00, 9.00)\n'
+py sinais.py receivers_data_example.json -v -46.9 -46.4 -41.2 -45.8 -48.7 \
+
--- sinais.py
+++ sinais.py
@@ -6,72 +6,77 @@ def main(argv: list[any]) -> None:
     Args:
         argv (list): command line arguments
     """
-    pivot = 0
+    pivot = 2
     
     if len(argv) <= 1:
         print_help()
         return
 
-    receivers_data = read_receivers_data(argv[1])
-    if not receivers_data:
+    receptors_data = read_receptors_data(argv[1])
+    if not receptors_data:
         return
     
-    A = get_coeficients_matrix(receivers_data, pivot)
+    A = get_coeficients_matrix(receptors_data, pivot)
     if not type(A) is np.ndarray:
         return
+    print(f"Receptor position coeficients matrix: \n{A}\n")
     
     potency_values = None
 
-    match argv[2]:
-        case "-v" | "--values":
-            potency_values = get_potency_values_arguments(argv[3:])
-        case "-i" | "--interactive":
-            potency_values = get_potency_values_input(len(receivers_data))
-        case _: # including -h | --help
-            print_help()
-            return
+    if argv[2] == "-v" or argv[2] == "--values":
+        potency_values = get_potency_values_arguments(argv[3:])
+    elif argv[2] == "-i" or argv[2] == "--interactive":
+        potency_values = get_potency_values_input(len(receptors_data))
+    else:
+        print_help()
+        return
         
     if potency_values == None:
         print("Error reading potency values")
         return
-    if len(potency_values) != len(receivers_data):
-        print(f"Invalid arguments: inadequate number of potency values for receivers (expected {len(receivers_data)})")
+    if len(potency_values) != len(receptors_data):
+        print(f"Invalid arguments: inadequate number of potency values for receptors (expected {len(receptors_data)})")
 
-    B = get_results_matrix(receivers_data, potency_values, pivot)
+    B = get_results_matrix(receptors_data, potency_values, pivot)
 
     if not type(B) is np.ndarray:
         return
+    print(f"System results matrix: \n{B}\n")
     
-    triangulate(A, B)
+    X = triangulate(A, B)
+
+    if type(X) is np.ndarray:
+        print(f"Estimated position: ({X.item(0)}, {X.item(1)})")
+    
 
 def print_help() -> None:
     """Prints usage info for the program"""
     print("""
 Usage: path command [values]
-Where path identifies the file with the receivers data and command is one of:
--v|--values       : parses the next n arguments and starts the program with n receivers and the
-                    values of the arguments as the potency received by the receivers
--i|--interactive  : starts the program in interactive mode, asking for the number of receivers
+Where path identifies the file with the receptors data and command is one of:
+-v|--values       : parses the next n arguments and starts the program with n receptors and the
+                    values of the arguments as the potency received by the receptors
+-i|--interactive  : starts the program in interactive mode, asking for the number of receptors
                     and the potency received values one by one
 -h|--help         : shows this help menu
     """)
 
 
-def read_receivers_data(file_name: str) -> list[dict[float, float, float, float]]:
-    """Tries to read the contents of a json file and returns the dict with the data of the receivers
+def read_receptors_data(file_name: str) -> list[dict[float, float, float, float]]:
+    """Tries to read the contents of a json file and returns the dict with the data of the receptors
 
     Args:
         file_name (str): name of the file to be read
 
     Returns:
         False: if the reading operation failed;
-        list[dict[float, float, float, float]]: the dicts containing the receivers data, following the structure:
+        list[dict[float, float, float, float]]: the dicts containing the receptors data, following the structure:
         [
             {
-                x: float - the x coordinate of the receiver
-                y: float - the y coordinate of the receiver
-                p0: float - reference potency value, measured 1 meter from the receiver
-                L: float - attenuation factor for the receiver
+                x: float - the x coordinate of the receptor
+                y: float - the y coordinate of the receptor
+                p0: float - reference potency value, measured 1 meter from the receptor
+                L: float - attenuation factor for the receptor
             }
         ], if the contents were succesfuly read.
     """
@@ -82,22 +87,22 @@ def read_receivers_data(file_name: str) -> list[dict[float, float, float, float]
                 return False
             
             f.seek(0)
-            receivers_data = json.load(f)
-            n_receivers = len(receivers_data)
+            receptors_data = json.load(f)
+            n_receptors = len(receptors_data)
 
-            if n_receivers <= 3:
-                print("Bad file format: at least 3 receivers are needed")
+            if n_receptors < 3:
+                print("Bad file format: at least 3 receptors are needed")
                 return False
 
-            for i in range(n_receivers):
-                if len(receivers_data[i]) < 4:
-                    x, y = receivers_data[i][0], receivers_data[i][1]
-                    print(f"Bad file format: receiver {i} (possibly at coords ({x}, {y})) does not have enough information")
+            for i in range(n_receptors):
+                if len(receptors_data[i]) < 4:
+                    x, y = receptors_data[i][0], receptors_data[i][1]
+                    print(f"Bad file format: receptor {i} (possibly at coords ({x}, {y})) does not have enough information")
                     return False
 
-            return receivers_data    
+            return receptors_data    
     except FileNotFoundError:
-        print("Invalid arguments: file with receivers data not found")
+        print("Invalid arguments: file with receptors data not found")
     except json.JSONDecodeError:
         print("Bad file format: invalid JSON")
     return False
@@ -123,19 +128,19 @@ def get_potency_values_arguments(arguments: list[any]) -> list[float]:
     return False
 
 
-def get_potency_values_input(n_receivers: int) -> list[float]:
+def get_potency_values_input(n_receptors: int) -> list[float]:
     """Starts interactive mode, which will ask for as much potency values as expected, and returns those values
 
     Args:
-        n_receivers (int): how many receivers there are, therefore how many values to ask for
+        n_receptors (int): how many receptors there are, therefore how many values to ask for
 
     Returns:
         list[float]: the potency values informed
     """
     potency_values = []
-    for i in range(n_receivers):
+    for i in range(n_receptors):
         while True:
-            print(f"Input the potency received value for the receiver {i + 1}: ")
+            print(f"Input the potency received value for the receptor {i + 1}: ")
             try:
                 potency_values.append(float(input()))
                 break
@@ -145,82 +150,90 @@ def get_potency_values_input(n_receivers: int) -> list[float]:
     return potency_values
 
 
-def get_coeficients_matrix(receivers_data: list[dict[float, float, float, float]],
+def get_coeficients_matrix(receptors_data: list[dict[float, float, float, float]],
                            pivot: float) -> np.ndarray:
-    """Generates a matrix (A) from the receivers data, based on their coordinates. This matrix comes
-    from the position equations for each receiver, contructed by the Pithagorean Theorem, of the form
+    """Generates a matrix (A) from the receptors data, based on their coordinates. This matrix comes
+    from the position equations for each receptor, contructed by the Pithagorean Theorem, of the form
         fk(x, y, z) = (x - xk)^2 + (y - yk)^2 = dk^2
     which are then expanded to
         fk(x, y, z) = x^2 - 2*x*xk + xk^2 + y^2 - 2*y*yk + yk^2 = dk^2
     and grouped as
         fk(x, y, z) = x^2 + xk^2 + y^2 + yk^2 - 2*(x*xk + y*yk) = ck
     finally, they are linearized using an arbitrary pivot number (e.g 1)
-        f1 - f2 = 2*x*(x2 - x1) + 2*y*(y2 - y1) = c2 - c1
-        f1 - f3 = 2*x*(x3 - x1) + 2*y*(y3 - y1) = c3 - c1
+        f2 - f1 = 2*x*(x1 - x2) + 2*y*(y1 - y2) = c2 - c1
+        f3 - f1 = 2*x*(x1 - x3) + 2*y*(y1 - y3) = c3 - c1
         ...
-        f1 - fm = 2x(xm - x1) + 2y(ym - y1) = c2 - c1
+        fm - f1 = 2x(x1 - xm) + 2y(y1 - ym) = cm - c1
     and represented in matrix notation
-        | 2*(x2 - x1)    2*(y2 - y1)|             |c2 - c1|
-        | 2*(x3 - x1)    2*(y3 - y1)|       |x|   |c3 - c1|
+        | 2*(x1 - x2)    2*(y1 - y2)|             |c2 - c1|
+        | 2*(x1 - x3)    2*(y1 - y3)|       |x|   |c3 - c1|
         |...                        |   x   |y| = |...    | <> A x X = B
-        | 2*(xm - x1)    2*(ym - y1)|       |z|   |cm - c1|
+        | 2*(x1 - xm)    2*(y1 - ym)|       |z|   |cm - c1|
 
     Args:
-        receivers_data (list[dict[float, float, float, float]]): the receivers data, including their coordinates
+        receptors_data (list[dict[float, float, float, float]]): the receptors data, including their coordinates
         pivot (float): the arbitrary pivot number to linearize the system
 
     Returns:
         np.ndarray: The Mx2 coeficients matrix
     """
     A = np.array([
-        [2 * (receivers_data[i]["x"] - receivers_data[pivot]["x"]), 2 * (receivers_data[i]["y"] - receivers_data[pivot]["y"])]
-        for i in range(len(receivers_data)) if i != pivot])
+        [2 * (receptors_data[pivot]["x"] - receptors_data[i]["x"]), 2 * (receptors_data[pivot]["y"] - receptors_data[i]["y"])]
+        for i in range(len(receptors_data)) if i != pivot])
     
     if A.shape[0] == A.shape[1] and np.linalg.det(A) == 0:
-        print("Bad receivers data: coeficient matrix based on the positions has null determinant")
+        print("Bad receptors data: coeficient matrix based on the positions has null determinant")
         return False
     
     return A
 
 
-def get_estimated_distance(receiver_data: dict[float, float, float, float], value: float):
-    """Calculates the estimated distance between a receiver and the emissor using the formula
+def get_estimated_distance(receptor_data: dict[float, float, float, float], value: float):
+    """Calculates the estimated distance between a receptor and the emissor using the formula
     
     d = 10^((p0 - p) / (10 * L))
 
     Where p0 is the expected potency value, p is the received potency value and L is the attenuation factor
 
     Args:
-        receiver_data (dict[float, float, float, float]): the receiver data dictionary, inclding the keys p0 and L
+        receptor_data (dict[float, float, float, float]): the receptor data dictionary, inclding the keys p0 and L
         value (float): the received potency value
 
     Returns:
         float: the estimated distance
     """
-    return 10**((receiver_data["p0"] - value) / (10 * receiver_data["L"]))
+    return 10 ** ((receptor_data["p0"] - value) / (10 * receptor_data["L"]))
 
 
-def get_results_matrix(receivers_data: list[dict[float, float, float, float]],
+def get_results_matrix(receptors_data: list[dict[float, float, float, float]],
                        potency_values: list[float],
                        pivot: int) -> np.ndarray:
     """Generates a matrix (B) with the results of the linearized equations, as seen on the summary for get_coeficients_matrix.
 
     Args:
-        receivers_data (list): a list of the receivers data
-        potency_values (list): the received potency values for each receiver
+        receptors_data (list): a list of the receptors data
+        potency_values (list): the received potency values for each receptor
         pivot (int): the arbitrary pivot number to linearize the system
 
     Returns:
         np.ndarray: The Mx1 results matrix
     """
-    d_pivot = get_estimated_distance(receivers_data[pivot], potency_values[pivot])
-    return np.array([get_estimated_distance(receivers_data[i], potency_values[i]) - d_pivot
-                  for i in range(len(receivers_data)) if i != pivot])
+    d_pivot = get_estimated_distance(receptors_data[pivot], potency_values[pivot]) ** 2 - receptors_data[pivot]["x"] ** 2 - receptors_data[pivot]["y"] ** 2
+    return np.array([(get_estimated_distance(receptors_data[i], potency_values[i]) ** 2 - receptors_data[i]["x"] ** 2 - receptors_data[i]["y"] ** 2) - d_pivot
+                  for i in range(len(receptors_data)) if i != pivot])
 
 
 def triangulate(A: np.ndarray, B: np.ndarray) -> np.ndarray:
-    print(A)
-    print(B)
+    """Triangulates the position of the emissor using the linearized system in matrix form
+
+    Args:
+        A (np.ndarray): Receptor position coeficients matrix
+        B (np.ndarray): System results matrix
+
+    Returns:
+        np.ndarray: 1D array with the estimated X and Y coordinates
+    """
+    return np.linalg.inv(A.T @ A) @ A.T @ B
 
 
 if __name__ == "__main__":
