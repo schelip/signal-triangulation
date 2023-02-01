5ddf5c9d4c3ac6a697030ef04ab3c8d33e21fa47
schelip <fcominscheffel@gmail.com>
2023-01-31 22:42:09 -0300

    rename main file

--- example.sh
+++ example.sh
@@ -33,8 +33,8 @@ echo '[
 ]' > receivers_data_example.json
 
 printf 'Exemplo 1 (3.00, 3.00)\n'
-py sinais.py receivers_data_example.json -v -48.4 -50.6 -32.2 -47.4 -46.3 \
+py signal_triangulation.py receivers_data_example.json -v -48.4 -50.6 -32.2 -47.4 -46.3 \
 
 printf '\nExemplo 2 (0.00, 9.00)\n'
-py sinais.py receivers_data_example.json -v -46.9 -46.4 -41.2 -45.8 -48.7 \
+py signal_triangulation.py receivers_data_example.json -v -46.9 -46.4 -41.2 -45.8 -48.7 \
 
